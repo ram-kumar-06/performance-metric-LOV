5ce6fbeb805493afebf9f22eb0bc8a755d18e96e
ram-kumar-06 <ramkumar.togapurkar@version1.com>
2025-06-04 10:10:22 -0800

    the first commit

---
+++ src/main/java/com/performance/kpis/list_of_values/ListofValuesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.performance.kpis.list_of_values;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListofValuesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListofValuesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/performance/kpis/list_of_values/ListofValuesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performance.kpis.list_of_values;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListofValuesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
